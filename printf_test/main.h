7186614de13140b7828bc62c6215aeb6dc28512c
maryprecious <maryprecious144@gmail.com>
2023-10-15 08:48:31 -0700

    print for print

---
+++ printf_test/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _putint(int in);
+int putchr(char c);
+int _printf(const char *format, ...);
+int dee_puts(char *st);
+#endif
---
+++ printf_test/test/2m.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ()
+{
+	int i;
+
+	i = printf("i am a boy");
+	printf("%d"' i);
+		return (0);
+}
